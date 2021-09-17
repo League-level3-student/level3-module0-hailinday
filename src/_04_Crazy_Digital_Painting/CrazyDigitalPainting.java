093170180c4ef9bff22aa51a77b63a54a3e344a6
hailinday <54258211+hailinday@users.noreply.github.com>
2021-09-17 16:54:32 -0700

    color palette

--- src/_04_Crazy_Digital_Painting/CrazyDigitalPainting.java
+++ src/_04_Crazy_Digital_Painting/CrazyDigitalPainting.java
@@ -20,16 +20,20 @@ public class CrazyDigitalPainting {
         //    to a new color. The sample image was created using the following 
         //    pattern:
         //    colors[i][j] = new Color(i % 256, (i * j) % 256, j % 256);
+    	//for (int i = 0; i < colors.length; i++) {
+		//	for (int j = 0; j < colors[i].length; j++) {
+		//		colors[i][j] = new Color(i % 256, (i * j) % 256, j % 256);
+		//	}
+		//}
+        // 5. Come up with your own pattern to make a cool crazy image.
     	for (int i = 0; i < colors.length; i++) {
 			for (int j = 0; j < colors[i].length; j++) {
-				colors[i][j] = new Color(i % 256, (i * j) % 256, j % 256);
+				colors[i][j] = new Color((i*2) % 256, (i * j) % 256, (j+i) % 256);
 			}
 		}
-        // 5. Come up with your own pattern to make a cool crazy image.
-
         // 6. Use the ColorArrayDisplayer class to call the displayColorsAsImage method 
         //    to show off your picture.
-    	
+    	ColorArrayDisplayer.displayColorsAsImage(colors);
     }
 
     public static void main(String[] args) {
